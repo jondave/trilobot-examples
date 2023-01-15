a6747528c46a07817d73ce9f073df72318accf29
Leonardo Guevara <guevaraleo92@gmail.com>
2023-01-15 23:02:22 +0000

    Update ball_tracking.py

--- scripts/ball_tracking.py
+++ scripts/ball_tracking.py
@@ -39,7 +39,7 @@ def circle_detection(image):
     # Apply Hough transform on the blurred image.
     detected_circles = cv2.HoughCircles(gray_blurred, 
                        cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
-                   param2 = 50, minRadius = 0, maxRadius = 0)
+                   param2 = 80, minRadius = 0, maxRadius = 0)
     # Count and locate the circles that are detected.
     x=[] # list with x component of the circles detected
     y=[] # list with y component of the circles detected
