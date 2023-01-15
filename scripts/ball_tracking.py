3fc16b61b0c3c70e87d58ebfbe3f4bef134c138e
Leonardo Guevara <guevaraleo92@gmail.com>
2023-01-15 23:32:15 +0000

    Update ball_tracking.py

--- scripts/ball_tracking.py
+++ scripts/ball_tracking.py
@@ -117,9 +117,9 @@ def activate_leds(color_wanted):
 
 def ball_tracking(x,w):
     err_x = x - w/2
-    vel = 0.5*(-float(err_x) / 100)
+    vel = 1*(-float(err_x) / 100)
     print("VELOCITY",vel)
-    if vel<0.1:
+    if vel<0.2:
         tbot.disable_motors()
     else:
         tbot.set_motor_speeds(vel, -vel)
