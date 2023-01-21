c365040a04c4688356e955fb0d45a1b7939fd681
Leonardo Guevara <guevaraleo92@gmail.com>
2023-01-21 10:28:39 +0000

    Update color_based_operation.py

--- scripts/color_based_operation.py
+++ scripts/color_based_operation.py
@@ -231,7 +231,7 @@ while True or KeyboardInterrupt:
             tbot.disable_motors()
     else:
         print("NO BALLS DETECTED")
-        if robot_action!="TRACKING THE BALL"
+        if robot_action!="TRACKING THE BALL":
             action_completed=False # To reset the robot operation
             print("ROBOT OPERATION RESETED") 
         tbot.fill_underlighting(BLACK)  
